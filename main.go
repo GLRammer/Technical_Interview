f9a57ade4b540631fbb05c0dfb32e8ef0a03c3a8
Gary Layman <45434125+GLRammer@users.noreply.github.com>
2025-10-03 15:58:41 -0700

    build output structure and finish fiber

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
     "log"
-
+    "time"
     "github.com/gofiber/fiber/v2"
 )
+
+type Output struct{
+    Message string `json:"message"`
+    Timestamp time.Time `json:"timestamp"`
+}
 
 func main() {
     app := fiber.New()
 
     app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
+        out := Output{Message: "My name is Gary Layman", Timestamp:time.Now()}
+        return c.JSON(out)
     })
 
     log.Fatal(app.Listen(":3000"))
