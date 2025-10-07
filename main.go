821dc1eb4f9b0d2138074c45f3a1da3f25a92fec
Gary Layman <45434125+GLRammer@users.noreply.github.com>
2025-10-07 09:43:07 -0700

    change timestamp to Unix time

--- main.go
+++ main.go
@@ -9,15 +9,15 @@ import (
 )
 
 type Output struct {
-	Message   string    `json:"message"`
-	Timestamp time.Time `json:"timestamp"`
+	Message   string `json:"message"`
+	Timestamp int64  `json:"timestamp"`
 }
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		out := Output{Message: "My name is Gary Layman", Timestamp: time.Now()}
+		out := Output{Message: "My name is Gary Layman", Timestamp: time.Now().Unix()}
 		outString, _ := json.Marshal(out)
 		return c.Send(outString)
 	})
